00a56ba23a3e3aff7bc46c5f5b3e2dc112bde690
zengqingcai@feitaikeji.com <zengqingcai@feitaikeji.com>
2020-05-14 14:39:34 +0800

    test分支练习

---
+++ user-common/src/main/java/com/user/common/utils/string/TestDev.java
@@ -0,0 +1,12 @@
+package com.user.common.utils.string;
+
+/**
+ * @Authod:zeng
+ * @Date: 2020/5/14 14:38
+ * @Version: 0.0.1
+ */
+public class TestDev {
+    public static void main(String[] args) {
+        System.out.println("Test Dev");
+    }
+}
